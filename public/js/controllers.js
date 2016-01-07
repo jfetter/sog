ef249d6b6df5fea2f867566c0c934453e75a5229
Jillian Fetter <jillian.fetter@gmail.com>
2016-01-07 15:26:24 -0800

    http request returns images from uifaces for background #6

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -2,12 +2,30 @@
 
 angular.module("sog")
 
-.controller("homeCtrl", function($scope, $state){
+.controller("homeCtrl", function($scope, $http){
 	console.log("inside home ctrl")
-	$scope.title = "HOME !!!!"
+	$scope.images = [];
+
+	//request random faces to populate background grid
+	// need to repeat request x times per row
+	// and repeat request for every row
+	// also need to appropriately size response
+	for(var i=0; i < 50; i ++){
+		$http({
+			method: "GET",
+			url: "http://uifaces.com/api/v1/random"
+				}).then(function(faces){
+					console.log(faces.data.image_urls.epic)
+				//find page-view-width and insert button at [index] page-view-width/128
+				$scope.images.push(faces.data.image_urls.epic)
+
+			}, function errorCallback(response){
+				console.log(response)
+			}) 
+	}
 })
 
-.controller("profileCtrl", function($scope, $state){
+.controller("profileCtrl", function($scope){
 	console.log("inside profile ctrl")
 	$scope.title = "PROFILE!!!!!!"
 })
